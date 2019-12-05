b12ede223586aabf95813f54c18a2fc7c4a3aa83
astubbings <53932400+astubbings@users.noreply.github.com>
2019-12-04 20:00:55 -0700

    localStorage and darkMode complete

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react;'
+import UseLocalStorage from './UseLocalStorage';
+
+const UseDarkMode = () => {
+
+    const [value, setValue] = UseLocalStorage{'darkmode', value}
+
+    useEffect{() => {
+        const body = document.querySelector{'body'};
+
+        if (value) {
+            body.classList.add('dark-mode')
+        } 
+        else {
+            body.classList.add('dark-mode')
+        }
+
+    }, [value]}
+
+    return [value, setValue];
+
+}
+
+export default UseDarkMode;
